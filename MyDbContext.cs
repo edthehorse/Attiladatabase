4575ef26df1f5b0f2876c9016e7111a294c56986
ed <edrobinson@hotmail.co.uk>
2021-01-09 00:42:45 +0000

    Add project files.

---
+++ MyDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Attiladatabase.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Attiladatabase
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=myAttila;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { }
+    }
+
+
+}
